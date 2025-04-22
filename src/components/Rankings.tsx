b12e56dc3eca8e8df55d26a29184c05acd833325
Jacob Seiler <seiler.jacob1@gmail.com>
2025-04-21 23:29:34 -0400

    Mobile friendly

--- src/components/Rankings.tsx
+++ src/components/Rankings.tsx
@@ -3,7 +3,7 @@ import VideoPreview from "./VideoPreview";
 
 const Rankings = ({ videos }: { videos: Video[] }) => {
     return (
-        <div className="grid grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 justify-items-center">
             {videos.map((video, i) => <VideoPreview key={video.id} video={video} rank={i + 1} />)}
         </div>
     );
